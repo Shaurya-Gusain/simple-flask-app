cbb8244425120153fd5665dfd60d688bcfc3a547
Shaurya-Gusain <shauryagusains@gmail.com>
2026-04-21 09:50:07 -0700

    test cd

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,4 +6,4 @@ docker pull shaurya45/simple-flask-app:latest
 
 echo "Starting new container..."
 
-docker run -d -p 5000:5000 --name flask-app shaurya45/simple-flask-app:latest
+docker run -d -p 5000:5000 --name flask-app shaurya45/simple-flask-app:latest 
